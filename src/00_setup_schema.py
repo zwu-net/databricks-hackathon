edada59661f298f955b5fc37a92e48f050f802f9
zwu-net <zwu.net@gmail.com>
2025-11-11 01:48:41 +0000

    Fix the setup error

--- src/00_setup_schema.py
+++ src/00_setup_schema.py
@@ -1,7 +1,5 @@
 # Databricks notebook source
-spark.sql("""
-CREATE CATALOG IF NOT EXISTS main;
-CREATE SCHEMA IF NOT EXISTS main.hackathon;
-CREATE VOLUME IF NOT EXISTS main.hackathon.bls_data;
-CREATE VOLUME IF NOT EXISTS main.hackathon.population_data;
-""")
+spark.sql("CREATE CATALOG IF NOT EXISTS main")
+spark.sql("CREATE SCHEMA IF NOT EXISTS main.hackathon")
+spark.sql("CREATE VOLUME IF NOT EXISTS main.hackathon.bls_data")
+spark.sql("CREATE VOLUME IF NOT EXISTS main.hackathon.population_data")
