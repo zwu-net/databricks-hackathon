66776a32afbb78932b431316beec3b03b0d5a053
zwu-net <zwu.net@gmail.com>
2025-11-10 17:45:22 -0800

    Final touch up.

---
+++ src/00_setup_schema.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+spark.sql("""
+CREATE CATALOG IF NOT EXISTS main;
+CREATE SCHEMA IF NOT EXISTS main.hackathon;
+CREATE VOLUME IF NOT EXISTS main.hackathon.bls_data;
+CREATE VOLUME IF NOT EXISTS main.hackathon.population_data;
+""")
